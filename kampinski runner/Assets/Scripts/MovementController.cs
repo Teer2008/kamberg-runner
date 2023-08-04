902aaac9962395d801cb50c15e0ce78e3eb9026f
Teer2008 <83593344+Teer2008@users.noreply.github.com>
2023-08-04 14:38:14 +0200

    kein spammen mehr

--- kampinski runner/Assets/Scripts/MovementController.cs
+++ kampinski runner/Assets/Scripts/MovementController.cs
@@ -4,7 +4,7 @@ public class MovementController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float jumpForce;
-
+    private bool isgrounded = true;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -12,14 +12,23 @@ public class MovementController : MonoBehaviour
 
     private void Update()
     {
-        if (PlayerControls.Instance.Jump)
+        if (PlayerControls.Instance.Jump && isgrounded)
         {
             Jump();
+            isgrounded = false;
         }
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isgrounded |= true;
+        }
     }
 }
