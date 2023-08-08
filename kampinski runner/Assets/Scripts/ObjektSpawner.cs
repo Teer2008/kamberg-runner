15a364ef21400de04a5d8fd524d7176896fd17c3
Kambergner <141366402+Kambergner@users.noreply.github.com>
2023-08-08 22:23:12 +0200

    Überarbeitung

--- kampinski runner/Assets/Scripts/ObjektSpawner.cs
+++ kampinski runner/Assets/Scripts/ObjektSpawner.cs
@@ -6,9 +6,6 @@ public class ObjektSpawner : MonoBehaviour
     public float spawnIntervall = 2f; // Der Zeitabstand zwischen den Spawns in Sekunden
     public int anzahlSpawns = 5; // Die Anzahl der Spawns, die stattfinden sollen
 
-    public float minYPosition = -5f; // Minimale y-Position für den Spawn
-    public float maxYPosition = 2f; // Maximale y-Position für den Spawn
-
     private int spawnCounter = 0;
     private float nextSpawnTime;
 
@@ -29,14 +26,6 @@ public class ObjektSpawner : MonoBehaviour
 
     void SpawnObjekt()
     {
-        float yPosition = Random.Range(minYPosition, maxYPosition);
-        GameObject spawnedObjekt = Instantiate(objektPrefab, new Vector3(10f, yPosition, 0f), Quaternion.identity);
-
-        BewegungVonRechtsNachLinksMitRollen bewegungsSkript = spawnedObjekt.GetComponent<BewegungVonRechtsNachLinksMitRollen>();
-        if (bewegungsSkript != null)
-        {
-            bewegungsSkript.geschwindigkeit = geschwindigkeit;
-            bewegungsSkript.rollGeschwindigkeit = rollGeschwindigkeit;
-        }
+        Instantiate(objektPrefab, transform.position, Quaternion.identity);
     }
 }
